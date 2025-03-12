e323528f1d6f75f0561cb8037b73857a93f98c56
sivaprasadnk <sivaprasadnk123@gmail.com>
2025-03-12 16:27:35 +0530

    search correction

--- lib/presentation/bloc/country_bloc/country_bloc.dart
+++ lib/presentation/bloc/country_bloc/country_bloc.dart
@@ -4,7 +4,6 @@ import 'package:country_app/domain/use_cases/add_to_local_db_usecase.dart';
 import 'package:country_app/domain/use_cases/get_country_offline_usecase.dart';
 import 'package:country_app/domain/use_cases/get_country_online_usecase.dart';
 import 'package:country_app/domain/use_cases/search_country_offline_usecase.dart';
-import 'package:country_app/domain/use_cases/search_country_online_usecase.dart';
 import 'package:country_app/presentation/bloc/country_bloc/country_event.dart';
 import 'package:country_app/presentation/bloc/country_bloc/country_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -61,71 +60,29 @@ class CountryBloc extends Bloc<CountryEvent, CountryState> {
 
   searchCountry(SearchCountryEvent event, Emitter<CountryState> emit) async {
     emit(state.copyWith(loadingState: true, errorState: false));
-
-    var connectivityResult = await Connectivity().checkConnectivity();
-    bool isOnline = !connectivityResult.contains(ConnectivityResult.none);
-
-    if (isOnline) {
-      var response = await locator<SearchCountryOnlineUsecase>().call(
-        event.query,
-      );
-      response.fold(
-        (exc) {
-          emit(
-            state.copyWith(
-              loadingState: false,
-              errorState: true,
-              error: exc.toString(),
-              countryModelList: [],
-            ),
-          );
-        },
-        (data) async {
-          emit(
-            state.copyWith(
-              loadingState: false,
-              errorState: false,
-              countryModelList: data,
-            ),
-          );
-
-          try {
-            await locator<AddToLocalDbUsecase>().call(data);
-          } catch (e) {
-            emit(
-              state.copyWith(
-                errorState: true,
-                error: 'Failed to save to local DB: ${e.toString()}',
-              ),
-            );
-          }
-        },
-      );
-    } else {
-      var response = await locator<SearchCountryOfflineUsecase>().call(
-        event.query,
-      );
-      response.fold(
-        (exc) {
-          emit(
-            state.copyWith(
-              loadingState: false,
-              errorState: true,
-              error: exc.toString(),
-              countryModelList: [],
-            ),
-          );
-        },
-        (data) {
-          emit(
-            state.copyWith(
-              loadingState: false,
-              errorState: false,
-              countryModelList: data,
-            ),
-          );
-        },
-      );
-    }
+    var response = await locator<SearchCountryOfflineUsecase>().call(
+      event.query,
+    );
+    response.fold(
+      (exc) {
+        emit(
+          state.copyWith(
+            loadingState: false,
+            errorState: true,
+            error: exc.toString(),
+            countryModelList: [],
+          ),
+        );
+      },
+      (data) {
+        emit(
+          state.copyWith(
+            loadingState: false,
+            errorState: false,
+            countryModelList: data,
+          ),
+        );
+      },
+    );
   }
 }
